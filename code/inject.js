2304f183ede2c3a3425a2e5de1931cf3d3d61d1c
A.Block <calexblock@gmail.com>
2019-09-01 11:34:08 +0300

    fix: <object .../> may have no body element

--- code/inject.js
+++ code/inject.js
@@ -1,7 +1,9 @@
 var $body = document.querySelector('body');
-chrome.runtime.sendMessage({type: 'inject'});
-chrome.runtime.onMessage.addListener(function (e) {
-    if ('enable' === e.type) {
-        $body.classList.toggle('x-ray', e.enable);
-    }
-});
+if ($body) {
+    chrome.runtime.sendMessage({type: 'inject'});
+    chrome.runtime.onMessage.addListener(function (e) {
+        if ('enable' === e.type) {
+            $body.classList.toggle('x-ray', e.enable);
+        }
+    });    
+}
